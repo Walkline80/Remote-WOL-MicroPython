aa6eab8401fd9e5b934b39b593664d1866cf062c
Walkline <walkline@163.com>
2021-01-15 18:09:39 +0800

    增加main文件

---
+++ main.py
@@ -0,0 +1,53 @@
+"""
+The MIT License (MIT)
+Copyright © 2021 Walkline Wang (https://walkline.wang)
+https://gitee.com/walkline/remote-wol-micropython
+"""
+from utils.utilities import Utilities
+from utils.wifihandler import WifiHandler
+from config import Config
+from utime import sleep
+
+
+forever_loop = True
+ap_server = None
+
+if __name__ == "__main__":
+	try:
+		if not Utilities.is_settings_file_exist():
+			# 进入用户配网模式
+			from services.web_server import WebServer
+
+			WifiHandler.set_ap_mode()
+			WifiHandler.set_sta_status(False)
+			sleep(1)
+
+			ap_server = WebServer(root_path=Config.AP_ROOTPATH)
+			ap_server.bind_address(Config.AP_HOST, Config.AP_PORT)
+			ap_server.start()
+
+			while ap_server.is_running():
+				sleep(0.5)
+		else:
+			# 进入工作模式
+			from hardware import Selector
+
+			WifiHandler.set_ap_status(False)
+			sleep(1)
+
+			if WifiHandler.STATION_CONNECTED == Utilities.connect_to_internet():
+				device = Selector.select(Config.HARDWARE_VERSION)
+				device.setup()
+				device.start()
+
+				while forever_loop:
+					sleep(0.5)
+			else:
+				# 600 秒后无法连接指定的 wifi 则重启
+				Utilities.hard_reset()
+	except KeyboardInterrupt:
+		forever_loop = False
+
+		print("\nPRESS CTRL+D TO RESET DEVICE")
+
+		if ap_server is not None: ap_server.stop()
