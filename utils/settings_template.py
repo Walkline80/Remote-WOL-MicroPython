bb7d8ea066ee3c06914899508562fc80ea2d4300
Walkline <walkline@163.com>
2021-02-03 00:15:20 +0800

    修复格式错误

--- utils/settings_template.py
+++ utils/settings_template.py
@@ -7,9 +7,9 @@ class Settings(object):
 	MQTT_HOST = "{mqtt_host}"
 	MQTT_PORT = {mqtt_port}
 	MQTT_KEEPALIVE = {mqtt_keepalive}
-	MQTT_BIGIOT_USERNAME = {mqtt_bigiot_username}
+	MQTT_BIGIOT_USERNAME = "{mqtt_bigiot_username}"
 	MQTT_IS_BIGIOT = {mqtt_is_bigiot}
-	MQTT_CLIENT_ID = {mqtt_client_id}
-	MQTT_USERNAME = {mqtt_username}
-	MQTT_PASSWORD = {mqtt_password}
+	MQTT_CLIENT_ID = "{mqtt_client_id}"
+	MQTT_USERNAME = "{mqtt_username}"
+	MQTT_PASSWORD = "{mqtt_password}"
 """
