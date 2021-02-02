a2a76f701c3dae54987fa0ffc3d5641f67879a21
Walkline <walkline@163.com>
2021-02-02 17:22:56 +0800

    调整 settings 模板参数

--- utils/settings_template.py
+++ utils/settings_template.py
@@ -7,9 +7,9 @@ class Settings(object):
 	MQTT_HOST = "{mqtt_host}"
 	MQTT_PORT = {mqtt_port}
 	MQTT_KEEPALIVE = {mqtt_keepalive}
-	MQTT_PATH = "{mqtt_path}"
-	MQTT_USERNAME = "{mqtt_username}"
-	MQTT_DEVICE_NUMBER = "{mqtt_device_number}"
-	MQTT_DEVICE_AUTHORIZE = "{mqtt_device_authorize}"
-	MQTT_DEVICE_NAME = "{mqtt_device_name}"
+	MQTT_BIGIOT_USERNAME = {mqtt_bigiot_username}
+	MQTT_IS_BIGIOT = {mqtt_is_bigiot}
+	MQTT_CLIENT_ID = {mqtt_client_id}
+	MQTT_USERNAME = {mqtt_username}
+	MQTT_PASSWORD = {mqtt_password}
 """
