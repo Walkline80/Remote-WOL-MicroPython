a8689698e65f6fcc9b1a7b509d1907ea5c8472f2
Walkline <walkline@163.com>
2021-03-04 16:58:45 +0800

    增加同步时间和重启设备命令相应代码

--- hardware/mqtt_sub_callback.py
+++ hardware/mqtt_sub_callback.py
@@ -7,6 +7,8 @@ import json
 from utils.wol import wake_on_lan
 from utils.utilities import Utilities
 from utils.wifihandler import WifiHandler
+from machine import RTC
+from utime import localtime
 import gc
 
 
@@ -26,24 +28,44 @@ class MQTTSubCallback(object):
 
 		try:
 			json_obj = json.loads(str(msg, "utf-8"))
+			command = json_obj['command']
 
-			if json_obj['command'] == "wake_up_pc":
-				wake_on_lan(json_obj['mac'])
+			if command == "wake_up_pc":
+				for count in range(3):
+					wake_on_lan(json_obj['mac'])
 
 				json_obj['command'] = 'wake_up_pc_result'
 				json_obj['result'] = 'success'
-
 				self._client.publish(topic, json.dumps(json_obj))
-			elif json_obj['command'] == 'device_remove':
+			elif command == 'device_remove':
 				if json_obj['mac'] == WifiHandler.get_mac_address():
-
 					json_obj['command'] = 'device_remove_result'
 					json_obj['result'] = 'success'
-
 					self._client.publish(topic, json.dumps(json_obj))
 					
 					Utilities.del_settings_file()
 					Utilities.hard_reset()
+			elif command == 'sync_datetime':
+				datetime = json_obj['datetime']
+
+				RTC().datetime((
+					datetime['year'],
+					datetime['month'],
+					datetime['day'],
+					datetime['weekday'], # 0~6
+					datetime['hour'],
+					datetime['minute'],
+					datetime['second'],
+					datetime['millisecond']
+				))
+
+				json_obj['command'] = 'sync_datetime_result'
+				json_obj['result'] = 'success'
+				self._client.publish(topic, json.dumps(json_obj))
+
+				print("datetime: %02d-%02d-%02d %02d:%02d:%02d" % ((localtime()[:-2])))
+			elif command == 'device_reboot':
+				Utilities.hard_reset()
 		except ValueError:
 			pass
 		except KeyError as ke:
